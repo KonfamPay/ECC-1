63675531e86126afd4758e0cf819d857350237ce
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-02 08:55:27 +0100

    Added the icons for OurGoals section

--- Components/IconContainer/index.tsx
+++ Components/IconContainer/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface IconContainerProps {
-    iconPath?: string;
+    iconPath: string;
 }
 
 const IconContainer : React.FC<IconContainerProps> = ({iconPath}) => {
     return (
-        <div className='w-full h-6 bg-gradient-to-br from-[#0B63C5] to-[#0953A4] rotate-[15deg]'>
-
+        <div className='w-[80px] h-[80px] bg-gradient-to-br from-[#0B63C5] to-[#0953A4] -rotate-[8deg] rounded-[16px] flex items-center justify-center'>
+            <Image src={iconPath} width={40} height={40} />
         </div>
     )
 }
