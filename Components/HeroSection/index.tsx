3572326f4519492306c0b5b65cb44f0c766be0ce
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-04 13:58:50 +0100

    Updated HeroSection component

--- Components/HeroSection/index.tsx
+++ Components/HeroSection/index.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="items-center justify-center mt-48 mx-auto">
-      <p>Make & Resolve</p>
+    <div className="items-center justify-center mx-auto bg-[#F1F7FE]">
+      <div className="pt-48">
+        <p className="items-center mx-auto">Make & <span className="text-[#0B63C5]">resolve</span></p>
+        <br/>
+        <p>complaints easily</p>
+      </div>
     </div>
   )
 }
