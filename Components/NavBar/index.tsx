76e35ee9a8070c488d789ce34ce5cdc29103e276
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-05-04 05:57:11 +0100

    Navbar component updated

--- Components/NavBar/index.tsx
+++ Components/NavBar/index.tsx
@@ -7,10 +7,10 @@ const index : React.FC = () => {
         <div className="flex items-center">
           <img src="./Images/logo.svg" className="ml-5 h-40 w-40 -my-10 -mx-10" alt="ECC's Logo" />
         </div>
-        <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <button type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
           <img src="./Images/Hamburger.svg" alt="" className="h-8 w-8"/>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+        <div className="hidden w-full md:block md:w-auto">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#0B63C5] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Complaints</a>
@@ -25,7 +25,12 @@ const index : React.FC = () => {
               <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#0B63C5] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">FAQs</a>
             </li>
             <li>
-              <a href="#"><img src="./Images/search.svg" alt="" className="-mt-1"/></a>
+              <a href="#"><img src="./Images/search.svg" alt="" className="-mt-1 block pr-4 pl-3" /></a>
+            </li>
+            <li>
+              <button className="bg-[#0B63C5]">
+                Make a complaint
+              </button>
             </li>
           </ul>
         </div>
