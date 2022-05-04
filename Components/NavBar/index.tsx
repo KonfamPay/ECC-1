544c309e9c0de81871b907a437b98f0c4b465281
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-05-04 05:42:32 +0100

    Navbar component updated

--- Components/NavBar/index.tsx
+++ Components/NavBar/index.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 fixed top-0 w-full">
+    <nav className="border-gray-200 px-2 sm:px-4 py-2.5 bg-white fixed top-0 w-full">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <div className="flex items-center">
-          <img src="./Images/logo.svg" className="ml-5 h-14 w-14" alt="ECC's Logo" />
+          <img src="./Images/logo.svg" className="ml-5 h-40 w-40 -my-10 -mx-10" alt="ECC's Logo" />
         </div>
         <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
-          <img src="./icons/Hamburger.svg" alt=""  className="h-8 w-8"/>
+          <img src="./Images/Hamburger.svg" alt="" className="h-8 w-8"/>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
