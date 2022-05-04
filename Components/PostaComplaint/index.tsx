df312434f2f15f8885b35764855fa2ec4f28bf04
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-04 15:28:45 +0100

    Updated PostaComplaint component

--- Components/PostaComplaint/index.tsx
+++ Components/PostaComplaint/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="mt-48 bg-black">
-      <p>Not getting refund response or <br/>replacement?</p>
+    <div className="mt-48 bg-black py-40 items-center justify-center text-center">
+      <p className="mx-auto text-white text-4xl font-semibold">Not getting refund response or <br/>replacement?</p>
+      <button className="mx-auto items-center justify-center text-center bg-blue-600 rounded-[10px] py-4 px-[45px] mt-10"><p className="mx-auto items-center justify-center text-center text-white font-semibold">Post a complaint</p></button>
     </div>
   )
 }
