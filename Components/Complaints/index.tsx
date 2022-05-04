8a81a6d950f26639c7b59f251a6647a90d1506eb
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-04 17:00:11 +0100

    Updated complaints component

--- Components/Complaints/index.tsx
+++ Components/Complaints/index.tsx
@@ -2,7 +2,88 @@ import React from 'react'
 
 const index : React.FC = props => {
   return (
-    <div>index</div>
+    <div className="pt-48 bg-[#0B63C5]">
+      <p className="mx-auto text-center text-4xl font-semibold text-white pb-32">Complaints from the <br/> people</p>
+      <div className="bg-white container">
+        <div className="pt-20 ml-16 mr-40">
+          <div className="space-y-10">
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+            <div>
+              <p className="text-left font-medium">An online vendor with insta handle @greggry.stores scammed me of #250,000 for intended for a weavon i never got</p>
+              <br />
+              <p className="-mt-4 font-extralight">as of now iam a distributor of pine tm from vizag andhra pradesh as of **** oct all our costumer bills got reversed and my money lost no responce from them as nearly ****/- lost because of them now there is no website of them...</p>
+              <p className="font-extralight mt-1 text-[12px]">Today — April 22, 2022 by <span className="text-[#0B63C5]">@indaboski</span></p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
