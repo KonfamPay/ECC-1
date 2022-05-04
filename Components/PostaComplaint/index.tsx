a669b7b24c46826817865ed703f6d688e57cb78e
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-04 16:17:17 +0100

    Attempting to align a div to an svg 😅

--- Components/PostaComplaint/index.tsx
+++ Components/PostaComplaint/index.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="mt-48 bg-black py-40 items-center justify-center text-center">
-      <p className="mx-auto text-white text-4xl font-semibold">Not getting refund response or <br/>replacement?</p>
-      <button className="mx-auto items-center justify-center text-center bg-blue-600 rounded-[10px] py-4 px-[45px] mt-10"><p className="mx-auto items-center justify-center text-center text-white font-semibold">Post a complaint</p></button>
+    <div className="mt-48 bg-white items-center justify-center text-center relative">
+      <img src="./Images/rectangle2.svg" alt="" className="absolute w-full h-auto"/>
+      <div className="absolute items-center justify-center text-center top-2/4 left-2/4">
+        <p className="text-white text-4xl font-semibold top-[200px] left-[300px]">Not getting refund response or <br/>replacement?</p>
+        <button className="items-center justify-center text-center bg-blue-600 rounded-[10px] py-4 px-[45px] mt-10 top-[250px] left-[300px]"><p className="mx-auto items-center justify-center text-center text-white font-semibold">Post a complaint</p></button>
+      </div>  
     </div>
   )
 }
