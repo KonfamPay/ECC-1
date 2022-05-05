6645a3ad4970a957b6a00523a637c90d2274523e
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-05 17:36:19 +0100

    Changed the tag to the semantic footer element instead of a div

--- Sections/Footer/index.tsx
+++ Sections/Footer/index.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 
 const Footer : React.FC = (props) => {
     return (
-        <div className="px-[40px] md:px-0 relative flex flex-col md:flex-row items-start md:-left-8 xl:left-0 md:pt-[72px] md:justify-center xl:justify-start">
+        <footer className="px-[40px] md:px-0 relative flex flex-col md:flex-row items-start md:-left-8 xl:left-0 md:pt-[72px] md:justify-center xl:justify-start">
             <img src="./Images/logo.svg" className="xl:ml-[100px] lg:mr-[115px] relative -left-[30px] md:left-0 md:-top-16 md:h-40 w-28 h-28 md:w-40" alt="ECC's Logo" />
             <div className="flex flex-row gap-[50px] lg:gap-[56px] md:mr-[66px] lg:mr-[150px] xl:mr-[235px] mb-[40px]">
                 <FooterList title='NAVIGATE THROUGH' items={["Home", "Complaints"] }/>
                 <FooterList title='SUPPORT' items={["Contact Us", "FAQs"] }/>
             </div>
             <FooterList title='TAKE ACTION' items={["Post a Complaint", "Talk to our Lawyers"] } areButtons={true}/>
-        </div>
+        </footer>
 
 
     )
