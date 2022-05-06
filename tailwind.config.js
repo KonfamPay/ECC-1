46c100eec3d85f83db86d190d69ef31ab85d3a58
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-06 18:27:03 +0100

    Updated numbers component and deleted xs media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}','./sections/**/*.{js,ts,jsx,tsx}'],
   theme: {
   extend: {},
-  screens: {
-    'xs': '320px',
-    // => @media (min-width: 576px)
-  },
   },
   variants: {
   extend: {},
