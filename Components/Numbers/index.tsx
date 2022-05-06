b307a375dbf4b7f6bb463a35016fda63e1b3f61d
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-06 17:00:31 +0100

    Updated numbers component

--- Components/Numbers/index.tsx
+++ Components/Numbers/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="mt-48 bg-[#010C18] pt-10 w-auto h-[687px]">
+    <div className="mt-48 bg-[#010C18] pt-10 h-[687px] w-fit md:w-fit xl:w-full lg:w-full">
       <p className="text-[40px] text-white text-center font-bold mt-[88px]">Our Numbers speak good deed about us</p>
-      <div className="space-x-[120px] flex flex-row mx-auto items-center justify-center mt-[122px] space">
+      <div className="sm:space-x-4 lg:space-x-[120px] flex flex-row mx-auto items-center justify-center mt-[122px]">
         <div className="w-auto">
           <p className="text-white font-extrabold text-[72px]">75K+</p>
           <p className="text-white">Complaints filled by customers</p>
