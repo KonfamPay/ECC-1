6cb73b7110735323549c2fd0ef433ae58d2617cf
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-07 20:27:38 +0100

    Made the OurGoalsSection responsive and added a padding to the PartnersSection

--- Sections/OurGoalsSection/index.tsx
+++ Sections/OurGoalsSection/index.tsx
@@ -7,7 +7,7 @@ const OurGoalsSection : React.FC = (props) => {
         {
             title: 'Protect our customers', 
             description: 'Safety & security tips, scam watch, guidelines to protect users from fraud and put an end to inustice.', 
-            iconPath: '/Images/sword.svg'
+            iconPath: '/Images/shield.svg'
         }, 
         {
             title: 'Help our customers', 
@@ -22,8 +22,8 @@ const OurGoalsSection : React.FC = (props) => {
 
     ]
     return (
-        <div className="text-[32px]">
-            <h1 className='font-medium poppinsFont max-w-[800px] text-center mx-auto mt-[72px] mb-[90px]'>We are solely bent on redefining customer-buyer relationships in Nigeria.</h1>
+        <div>
+            <h1 className=' text-[22px] px-[30px] md:text-[32px] font-medium poppinsFont max-w-[800px] text-center mx-auto mt-[72px] mb-[90px]'>We are solely bent on redefining customer-buyer relationships in Nigeria.</h1>
             <div className='mt-[50px] flex flex-col lg:flex-row justify-center items-center px-[30px] lg:px-0 gap-[20px] mb-[114px]'>
                 {goalCardInfo.map(card => <GoalCard title={card.title} description={card.description} iconPath={card.iconPath}  />)}
             </div>
--- Sections/PartnersSection/index.tsx
+++ Sections/PartnersSection/index.tsx
@@ -2,7 +2,7 @@ import PartnerBox from "../../Components/PartnerBox";
 
 const PartnersSection : React.FC = (props) => {
     return (
-        <div>
+        <div className="pt-[100px] pb-[174px]">
             <h1 className='max-w-[500px] lg:max-w-[700px] text-[32px] lg:text-[40px] font-semibold poppinsFont mt-[100px] mb-[56px] text-center mx-auto'>Active Partners helping  to create safety and awareness</h1>
             <div className="flex flex-col gap-[16px] lg:gap-[32px] lg:scale-95">
                 <div className="flex flex-row justify-center items-center w-full md:gap-[17px] lg:gap-[28px]">
