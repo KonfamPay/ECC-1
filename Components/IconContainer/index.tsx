6c549461c2db5f7ae01b7be23e71e0f6f6ff832e
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-08 22:27:38 +0100

    Added size prop to the icon container

--- Components/IconContainer/index.tsx
+++ Components/IconContainer/index.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 interface IconContainerProps {
     iconPath: string;
-    size: string;
+    size?: number;
 }
 
 const IconContainer : React.FC<IconContainerProps> = ({iconPath, size}) => {
     return (
-        <div className={`w-[${!size ? "80" : size }px] h-[${!size ? "80" : size }px] bg-gradient-to-br from-[#0B63C5] to-[#0953A4] -rotate-[8deg] rounded-[16px] flex items-center justify-center`}>
-            <Image src={iconPath} width={40} height={40} />
+        <div className={`w-[${size ? size : "80" }px] h-[${size ? size : "80" }px] bg-gradient-to-br from-[#0B63C5] to-[#0953A4] -rotate-[8deg] rounded-[16px] flex items-center justify-center`}>
+            <Image src={iconPath} width={size ? size/2 : 40} height={size ? size/2 : 40}/>
         </div>
     )
 }
