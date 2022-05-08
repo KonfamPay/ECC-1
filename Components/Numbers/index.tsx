94ce4509dfa19579b30cc0304b0128d704c7120a
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-08 20:04:50 +0100

    Added responsiveness to numbers component

--- Components/Numbers/index.tsx
+++ Components/Numbers/index.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="mt-48 bg-[#010C18] pt-10 h-[687px] w-full sm:w-full md:w-full xl:w-full lg:w-full">
-      <p data-aos="fade-up" className="text-[40px] text-white text-center font-bold mt-[88px]">Our Numbers speak good deed about us</p>
-      <div className="space-x-2 sm:space-x-10 md:space-x-10 lg:space-x-[120px] xl:space-x-[120px] flex flex-row mx-auto items-center justify-center mt-[122px]">
+    <div className="mt-48 bg-[#010C18] pt-10 h-[687px] w-full">
+      <p data-aos="fade-up" className="text-[40px] text-white text-center font-bold mt-[20px] sm:mt-[88px]">Our Numbers speak good deed about us</p>
+      <div className="sm:space-x-10 md:space-x-10 lg:space-x-[120px] xl:space-x-[120px] flex sm:flex-row flex-col gap-5 mx-auto items-center justify-center mt-[36px] sm:mt-[122px]">
         <div data-aos="zoom-out" className="w-auto">
-          <p className="text-white text-center font-extrabold text-[30px] sm:text-[50px] md:text-[58px] xl:text-[72px]">75K+</p>
-          <p className="text-white text-center font-extralight text-[13px] md:text-[15px] lg:text-[20px]">Complaints filled <br/>by customers</p>
+          <p className="text-white text-center font-extrabold text-[35px] sm:text-[50px] md:text-[58px] xl:text-[72px]">75K+</p>
+          <p className="text-white text-center font-extralight text-[15px] sm:text-[13px] md:text-[15px] lg:text-[20px]">Complaints filled <br/>by customers</p>
         </div>
         <div data-aos="zoom-out" className="w-auto">
-          <p className="text-white text-center font-extrabold text-[30px] sm:text-[50px] md:text-[58px] xl:text-[72px]">56K+</p>
-          <p className="text-white text-center font-extralight text-[13px] md:text-[15px] lg:text-[20px]">Complains resolved <br/> by companies</p>
+          <p className="text-white text-center font-extrabold text-[35px] sm:text-[50px] md:text-[58px] xl:text-[72px]">56K+</p>
+          <p className="text-white text-center font-extralight text-[15px] sm:text-[13px] md:text-[15px] lg:text-[20px]">Complains resolved <br/> by companies</p>
         </div>
         <div data-aos="zoom-out" className="w-auto">
-          <p className="text-white text-center font-extrabold text-[30px] sm:text-[50px] md:text-[58px] xl:text-[72px]">200K+</p>
-          <p className="text-white text-center w-auto font-extralight text-[13px] md:text-[15px] lg:text-[20px]">Ever Ready Law <br/> Practitioners</p>
+          <p className="text-white text-center font-extrabold text-[35px] sm:text-[50px] md:text-[58px] xl:text-[72px]">200K+</p>
+          <p className="text-white text-center font-extralight text-[15px] sm:text-[13px] md:text-[15px] lg:text-[20px]">Ever Ready Law <br/> Practitioners</p>
         </div>
       </div>
     </div>
