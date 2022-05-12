c299f490073e7e97070d7ad6e828ab1b60fe8ad9
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-12 23:17:17 +0100

    Fixed ui responsiveness bug in the VentComplaintsCard

--- Components/VentComplaintsCard/index.tsx
+++ Components/VentComplaintsCard/index.tsx
@@ -7,7 +7,7 @@ interface VentComplaintsCardProps {
 const VentComplaintsCard : React.FC<VentComplaintsCardProps> = ({title, description, opacity}) => {
     return (
         <div data-aos="zoom-in-up" style={{opacity}} className="w-full bg-white bg-opacity-10 rounded-[16px] flex flex-row items-center px-[14px] sm:px-[24px] gap-[20px] py-[20px] sm:py-[30px]">
-            <div className="min-w-[60px] w-[60px] sm:w-[72px] h-[60px] sm:h-[72px] rounded-full border-[0.4px] border-[#C4C4C4]"></div>
+            <div className="min-w-[60px] sm:min-w-[72px] w-[60px] sm:w-[72px] h-[60px] sm:h-[72px] rounded-full border-[0.4px] border-[#C4C4C4]"></div>
             <div className="max-w-[328px]">
                 <h3 className='text-[19px] sm:text-[22px] font-semibold poppinsFont text-white'>{title}</h3>
                 <p className="text-white text-[14px] sm:text-[16px] poppinsFont">{description}</p>
