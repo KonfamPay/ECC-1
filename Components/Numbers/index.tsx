dacd47bb9543c41feb307cc6cd2fdb263144e4b0
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-12 15:18:16 +0100

    Made the numbers section responsive

--- Components/Numbers/index.tsx
+++ Components/Numbers/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="mt-48 bg-[#010C18] pt-10 h-[687px] w-full">
-      <p data-aos="fade-up" className="text-[40px] text-white text-center font-bold mt-[20px] sm:mt-[88px]">Our Numbers speak good deed about us</p>
+    <div className="mt-48 bg-[#010C18] pb-[80px] md:pb-0 pt-10 md:h-[687px] w-full">
+      <p data-aos="fade-up" className="text-[26px] px-[15px] md:text-[40px] text-white text-center font-bold mt-[20px] sm:mt-[88px]">Our Numbers speak good deed about us</p>
       <div className="sm:space-x-10 md:space-x-10 lg:space-x-[120px] xl:space-x-[120px] flex sm:flex-row flex-col gap-5 mx-auto items-center justify-center mt-[36px] sm:mt-[122px]">
         <div data-aos="zoom-out" className="w-auto">
           <p className="text-white text-center font-extrabold text-[35px] sm:text-[50px] md:text-[58px] xl:text-[72px]">75K+</p>
