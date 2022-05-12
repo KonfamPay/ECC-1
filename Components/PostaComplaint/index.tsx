e6790d0676901f6bd7517ee436683eca377eb0e0
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-12 15:03:10 +0100

    Made the partners section responsive

--- Components/PostaComplaint/index.tsx
+++ Components/PostaComplaint/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const index : React.FC = () => {
   return (
-    <div className="mt-48 bg-white items-center justify-center text-center relative">
+    <div className="lg:mt-48 bg-white items-center justify-center text-center relative">
       <img src="./Images/rectangle2.svg" alt="" className="w-full h-full"/>
       <div data-aos="zoom-in" className="absolute w-full h-full items-center justify-center text-center top-0 flex flex-col">
         <p className="text-white font-semibold text-[15px] md:text-[30px] lg:text-[40px] xl:text-[48px]">Not getting refund response or <span className="block sm:pt-2 lg:pt-5">replacement?</span></p>
