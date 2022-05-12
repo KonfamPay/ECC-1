ef611eb8dcc937c29a0f1878f6f9710457d3255b
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-12 10:29:15 +0100

    fixed the iconContainer component

--- Components/IconContainer/index.tsx
+++ Components/IconContainer/index.tsx
@@ -8,7 +8,7 @@ interface IconContainerProps {
 
 const IconContainer : React.FC<IconContainerProps> = ({iconPath, size}) => {
     return (
-        <div className={`w-[${size ? size : "80" }px] h-[${size ? size : "80" }px] bg-gradient-to-br from-[#0B63C5] to-[#0953A4] -rotate-[8deg] rounded-[16px] flex items-center justify-center`}>
+        <div className={`w-[80px] h-[80px] bg-gradient-to-br from-[#0B63C5] to-[#0953A4] -rotate-[8deg] rounded-[16px] flex items-center justify-center`}>
             <Image src={iconPath} width={size ? size/2 : 40} height={size ? size/2 : 40}/>
         </div>
     )
