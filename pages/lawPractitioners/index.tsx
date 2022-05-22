798bbaa68e9e44102510e5a1e107735e5b395fe4
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-22 14:59:14 +0100

    Created the LawPractitioners Page

---
+++ pages/lawPractitioners/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import NavBar from '../../Components/NavBar';
+
+const LawPracitioners : NextPage = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+}
+export default LawPracitioners;
