568c65d564961acda5722304910083c936878e2d
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-22 16:09:43 +0100

    Finished working on the MakeThingsRightSection

--- pages/lawPractitioners/index.tsx
+++ pages/lawPractitioners/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next';
 import NavBar from '../../Components/NavBar';
+import MakeThingsRightSection from '../../Sections/MakeThingsRightSection';
+import ResolveComplaintsSection from '../../Sections/ResolveComplaints';
 
 const LawPracitioners : NextPage = () => {
     return (
-        <div>
+        <div className='lg:mt-[200px] lg:mb-[150px]'>
             <NavBar />
+            <ResolveComplaintsSection />
+            <MakeThingsRightSection />
         </div>
     );
 }
