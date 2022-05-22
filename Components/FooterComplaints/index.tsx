7651d90c4422d155f20d19b6ac0ffca5cdb3c4d5
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-22 15:17:42 +0100

    Working on footeer complaints component

--- Components/FooterComplaints/index.tsx
+++ Components/FooterComplaints/index.tsx
@@ -3,8 +3,13 @@ import Image from 'next/image';
 
 const IconContainer : React.FC = () => {
     return (
-        <div className="bg-blue">
-          How far
+        <div className="h-[710px] w-auto">
+          <div className="pl-[140px] pt-10">
+            <img src="./Images/logo.svg" alt="" className="h-32 w-32"/>
+          </div>
+          <div className="pl-[568px] -mt-[75px]">
+              aedjend
+          </div>
         </div>
     )
 }
