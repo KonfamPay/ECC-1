00e65d307924bfdec5d6b4a1828bfcb77f899e2c
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-22 17:46:45 +0100

    Updated scam statistics component

--- pages/ScamStatistics/index.tsx
+++ pages/ScamStatistics/index.tsx
@@ -23,14 +23,30 @@ const index : NextPage = props => {
               <input type="text" className='mt-[12px] ml-[28px] mb-[27px] bg-[#F0F0F0] rounded-[12px] px-[86px] py-5 text-left text-[#666666]' placeholder='2022(Present Year)'/>
             </div>
             <div className='pt-[51px]'>
-              <input type="button" className='mt-[12px] ml-[28px] mb-[27px] bg-[#031830] rounded-[12px] px-[44px] py-5 text-left text-white' value={'View Stats'}/>
+              <input type="button" className='mt-[12px] ml-[28px] mb-[27px] bg-[#031830] rounded-[12px] px-[44px] py-5 text-left text-white cursor-pointer' value={'View Stats'}/>
             </div>
           </div>
         </form>
       </div>
       <div className='bg-[#F1F7FE] -mt-[90px]'>
         <div className='pt-[90px] pl-[140px]'>
-          <p className=''>Showing All Scam types stats for 2022</p>
+          <div>
+            <p className='text-[24px]'>Showing <span className='text-[#0B63C5]'>All Scam types</span>  stats for <span className='text-[#0B63C5]'>2022</span></p>
+          </div>
+          <div className='flex flex-row w-auto h-auto bg-white mr-[141px] rounded-[12px] mt-[20px] pb-[39px]'>
+            <div>
+              <p className='text-[25px] font-semibold pt-[40px] pl-[25px]'>$350,000,000</p>
+              <p className='pl-[25px] font-light pt-[12px]'>Amount Lost</p>
+            </div>
+            <div>
+              <p className='text-[25px] font-semibold pt-[40px] pl-[25px]'>$350,000,000</p>
+              <p className='pl-[25px] font-light pt-[12px]'>Amount Lost</p>
+            </div>
+            <div>
+              <p className='text-[25px] font-semibold pt-[40px] pl-[25px]'>$350,000,000</p>
+              <p className='pl-[25px] font-light pt-[12px]'>Amount Lost</p>
+            </div>
+          </div>
         </div>
       </div>
       <FooterComplaints/>
