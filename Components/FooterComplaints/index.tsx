ee1f91c2dd6c9148ea4da20c0a145ed9548b266f
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-05-22 15:06:08 +0100

    Created footercomplaints component

---
+++ Components/FooterComplaints/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+const IconContainer : React.FC = () => {
+    return (
+        <div className="bg-blue">
+          How far
+        </div>
+    )
+}
+
+export default IconContainer;
