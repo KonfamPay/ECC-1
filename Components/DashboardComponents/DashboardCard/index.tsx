082ecc2f25de7bd524341b5697b7c7bb59c94a20
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-18 13:45:36 +0100

    Chore: Added files for dashboard component

---
+++ Components/DashboardComponents/DashboardCard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+import { NextPage } from 'next';
+
+const LandingPageCard : NextPage = () => {
+    return (
+        <div className="h-[710px] w-auto">
+        </div>
+    )
+}
+
+export default LandingPageCard;
---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import DashboardCard from '../../Components/DashboardComponents/DashboardCard';
+
+const index : NextPage = props => {
+  return (
+    <div className="">
+      <DashboardCard/>
+    </div>
+  )
+}
+
+export default index
