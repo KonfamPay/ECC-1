1f0b8402403798e377f3b54a4d6b825a99d1db09
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-07-18 15:10:59 +0100

    Chore: Done ✅ with dashboard page

--- Components/DashboardComponents/DashboardCard/index.tsx
+++ Components/DashboardComponents/DashboardCard/index.tsx
@@ -4,19 +4,47 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 
 const LandingPageCard : NextPage = () => {
-	const cardcontents = [{
+	const cardcontents = [
+		{
 		iconpath: '../../../icons/dashboard-icons/my-complaints.svg',
-		title: '',
-		description: '',
-	}];
+		title: 'My Complaints',
+		description: 'Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ',
+		bgcolor: '#0B63C5'
+		},
+		{
+		iconpath: '../../../icons/dashboard-icons/help.svg',
+		title: 'Help',
+		description: 'Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ',
+		bgcolor: '#FFB330'
+		},
+		{
+		iconpath: '../../../icons/dashboard-icons/profile.svg',
+		title: 'My Portfolio',
+		description: 'Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ',
+		bgcolor: '#FF88B3'
+		},
+		{
+		iconpath: '../../../icons/dashboard-icons/fac.svg',
+		title: 'File a Complaint',
+		description: 'Lorem ipsum dolor sit amet, cctetur adipiscing elit ut ',
+		bgcolor: '#020D1B'
+		},
+	];
 	return (
 		<>
-			{cardcontents.map((card) => 
-			<div className="h-[710px] w-auto">
-				<div className='bg-white rounded-[27.2px]'>
-					<Link href=''><img src={card.iconpath} alt="" /></Link>
+			<div className='grid grid-cols-2 gap-x-[68px] gap-y-[37px]'>
+				{cardcontents.map((card) => 
+				<div className='flex flex-row pl-[32px] py-[55.5px] text-white rounded-[20px]' style={{backgroundColor: card.bgcolor}}>
+					<div className='rounded-[27.2px] bg-white px-[16px] py-[16px]'>
+						<img src={card.iconpath} className="w-[48px] h-[48px]" alt="" />
+					</div>
+					<div className='pl-[32px]'>
+						<p className='text-[24px] font-[600]'>{card.title}</p>
+						<p className='w-[363px]'>{card.description}</p>
+					</div>
 				</div>
-			</div>)}
+				)}
+			</div>
 		</>
 	)
 }
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,8 +3,8 @@ import DashboardCard from '../../Components/DashboardComponents/DashboardCard';
 
 const index : NextPage = props => {
   return (
-    <div className="">
-      <div className='bg-[#020D1B] mx-[28.5px] w-[1091px] h-[255px] rounded-[20px] flex flex-row relative'>
+    <div className="mx-[28.5px] mt-[55px]">
+      <div className='bg-[#020D1B] w-[1091px] h-[255px] rounded-[20px] flex flex-row relative'>
         <div className='text-white pl-[43px] py-[58.5px]'>
           <p className='text-[40px] leading-[60px] font-[600]'>Hi, Voke</p>
           <p className='text-[24px] leading-[36px] font-[600] w-[503px]'>Vendor don do you strong thing again? We are ready to listen to your complaints</p>
@@ -13,7 +13,7 @@ const index : NextPage = props => {
           <img src="./Images/man-freaked-out.png" className='absolute right-[21.28px] -top-[30.54px] h-[285.54px] w-[377.72px]' alt="" />
         </div>
       </div>
-      <div className='mt-[59px] mx-[28.5px]'>
+      <div className='mt-[59px]'>
         <DashboardCard/>
       </div>
     </div>
