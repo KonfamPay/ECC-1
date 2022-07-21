966724c23d6bff254025221e19a2d3c3d64cf065
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-21 14:44:26 +0100

    Chore: Created files for mycomplaints

--- pages/dashboard/mycomplaints/index.tsx
+++ pages/dashboard/mycomplaints/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next';
-import ComplaintsNavBar from '../../../Components/DashboardComponents/DashboardProfile/ComplaintsNavBar';
 
-const mycomplaints : NextPage = props => {
+const complaintDetails : NextPage = props => {
   return (
     <div className="mx-[28.5px] mt-[55px]">
-      <ComplaintsNavBar/>
+      
     </div>
   )
 }
 
-export default mycomplaints
+export default complaintDetails
