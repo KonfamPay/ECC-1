24316dd9ffc6ab4beb767ea95804b762e452a4e8
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-21 11:05:30 +0100

    Chore: Created new files and folders for compplaints navbar

---
+++ Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const complaintsnavbar : NextPage = props => {
+  return (
+    <div className="mx-[28.5px] mt-[55px]">
+      
+    </div>
+  )
+}
+
+export default complaintsnavbar
