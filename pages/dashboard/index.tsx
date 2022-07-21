bfe2cd78e4ed13bcba91a27e7a271e1b00bf0ae9
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-07-21 20:40:01 +0100

    Created the dashboard page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import NavWrapper from "../../Components/DashboardNav/NavWrapper";
+import SideNav from "../../Components/DashboardNav/SideNav";
+import TopNav from "../../Components/DashboardNav/TopNav";
+
+const Dashboard: NextPage = () => {
+  return (
+    <NavWrapper>
+      {/* <div className="w-full py-[45px] bg-white">
+        <div className="w-[80px] h-[80px] bg-[#FFFCB5] rounded-[27.2px]"></div>
+      </div> */}
+    </NavWrapper>
+  );
+};
+export default Dashboard;
