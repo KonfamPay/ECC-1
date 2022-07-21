5748e8bf9b08f01270f1b5ab41450b2e3d895981
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-21 10:38:12 +0100

    Chore: Added dummy profile image and updated profilecard

--- Components/DashboardComponents/DashboardProfile/ProfileCard/index.tsx
+++ Components/DashboardComponents/DashboardProfile/ProfileCard/index.tsx
@@ -5,44 +5,61 @@ const profile : NextPage = props => {
   return (
     <div className="ml-[31px] mt-[58px] w-[592px] bg-white rounded-[20px]">
       <img src="/icons/dashboard-icons/profile-header.svg" className='rounded-t-[20px]' alt="" />
+      <div>
+        <img src="imaf" alt="" />
+      </div>
       <div className='text-center'>
         <p className='mt-[94px] text-[24px] font-[500]'>Voke Onoriode</p>
         <p className='mt-[10px] text-[18px] font-[500]'>Vokeonoriode@gmail.com</p>
       </div>
-      <div className='mx-[74.5px] mt-[29px] grid grid-cols-2 gap-y-[86px] gap-x-[81px]'>
-        <div>
-          <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[165px]'>
-            <p className='text-[#0B63C5] text-[16px]'>Phone Number</p>
+      <div className='mx-[74.5px] mt-[29px] flex flex-col space-y-[96px]'>
+        <div className='flex flex-row space-x-[156px]'>
+          <div>
+            <div>
+              <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[165px]'>
+                <p className='text-[#0B63C5] text-[16px]'>Phone Number</p>
+              </div>
+              <div className='mt-[10px]'>
+                <p className='text-[18px]'>09065748390</p>
+              </div>
+            </div>
           </div>
-          <div className='mt-[10px]'>
-            <p className='text-[18px]'>09065748390</p>
-          </div>
-        </div>
-        <div>
-          <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[165px]'>
-            <p className='text-[#0B63C5] text-[16px]'>Date of Birth</p>
-          </div>
-          <div className='mt-[10px]'>
-            <p className='text-[18px]'>12-01-2001</p>
-          </div>
-        </div>
-        <div>
-          <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[165px]'>
-            <p className='text-[#0B63C5] text-[16px]'>Address</p>
-          </div>
-          <div className='mt-[10px]'>
-            <p className='text-[18px]'>4. jubril Adejumobi Street
-Lagos, Nigeria</p>
+          <div>
+            <div>
+              <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[122px]'>
+                <p className='text-[#0B63C5] text-[16px]'>Date of Birth</p>
+              </div>
+              <div className='mt-[10px]'>
+                <p className='text-[18px]'>12-01-2001</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-          <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[165px]'>
-            <p className='text-[#0B63C5] text-[16px]'>State</p>
+        <div className='flex flex-row space-x-[81px]'>
+          <div>
+            <div>
+              <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[240px]'>
+                <p className='text-[#0B63C5] text-[16px]'>Address</p>
+              </div>
+              <div className='mt-[10px]'>
+                <p className='text-[18px] w-[219px]'>4. jubril Adejumobi Street Lagos, Nigeria</p>
+              </div>
+            </div>
           </div>
-          <div className='mt-[10px]'>
-            <p className='text-[18px]'>Lagos</p>
+          <div>
+            <div>
+              <div className='border-b-[0.5px] border-b-[#C5C5C5] w-[103px]'>
+                <p className='text-[#0B63C5] text-[16px]'>State</p>
+              </div>
+              <div className='mt-[10px]'>
+                <p className='text-[18px]'>Lagos</p>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <div className='w-[446px] bg-[#0B63C5] mx-[73px] rounded-[12px] mt-[70px]'>
+        <p className='text-center text-white font-[600] py-[16.5px]'>Edit</p>    
       </div>
     </div>
   )
