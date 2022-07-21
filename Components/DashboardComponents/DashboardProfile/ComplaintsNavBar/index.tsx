6867d92e4db0cefff712aa8a50355857e66b2bd3
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-21 11:55:04 +0100

    Chore: Added hover functionality to complaintsnavbar

--- Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
+++ Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
@@ -29,7 +29,7 @@ const complaintsnavbar : NextPage = props => {
     <div className="mx-[35px] mt-[44px] bg-[#0B63C5] rounded-t-[15px] h-[104px] w-[1079px]">
       <div className='mx-[76.5px] text-white py-[23.5px] flex flex-row space-x-[60px]'>
         {complaintsnavbarcontents.map((content) => 
-          <div className='border-2 border-white w-fit h-[57px] rounded-[40px] text-center'>
+          <div className='border-2 border-white w-fit h-[57px] rounded-[40px] text-center hoverAnimation'>
             <a href={content.link}>
               <p className='py-[15px] px-[40px] text-[18px]'>{content.text}</p>
             </a>
