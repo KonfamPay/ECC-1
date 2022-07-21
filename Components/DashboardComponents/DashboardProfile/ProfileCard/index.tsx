0a0f2e298582f1e68b3ec066cb9ac4861fcd3e16
Adetunji Peter Adeyinka <adetunjiadeyinka@Adetunjis-MacBook-Air.local>
2022-07-20 19:42:54 -0700

    Chore: Added profile header and created new directories for dasjboard-profile

---
+++ Components/DashboardComponents/DashboardProfile/ProfileCard/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import DashboardCard from '../../DashboardCard';
+
+const profile : NextPage = props => {
+  return (
+    <div className="ml-[31px] mt-[58px] w-[592px] bg-white">
+      
+    </div>
+  )
+}
+
+export default profile
