1c49c6ecd54033a851ae1410398003363e245628
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-21 11:14:58 +0100

    Chore: Updated complaints navbar

--- Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
+++ Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from 'next';
 
 const complaintsnavbar : NextPage = props => {
   return (
-    <div className="mx-[28.5px] mt-[55px]">
-      
+    <div className="mx-[35px] mt-[44px] bg-[#0B63C5] rounded-t-[15px] h-[104px] w-[1079px]">
+      <div className='mx-[76.5px] text-white py-[23.5px]'>
+        <div className='border-2 border-white w-[102px] h-[57px] rounded-[40px] text-center'>
+          <p className='py-[15px]'>All</p>
+        </div>
+      </div>
     </div>
   )
 }
