f9e66c817faa44e4217cb33cccbb3cce0d6aa9c5
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-21 12:08:17 +0100

    Chore: Completed complaintsnavbar component ✅

--- Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
+++ Components/DashboardComponents/DashboardProfile/ComplaintsNavBar/index.tsx
@@ -5,23 +5,28 @@ const complaintsnavbar : NextPage = props => {
   const complaintsnavbarcontents = [
     {
       text: "All",
-      link: ""
+      active: true,
+      link: ''
     },
     {
       text: "Resolved",
-      link: ""
+      active: false,
+      link: ''
     },
     {
       text: "Open",
-      link: ""
+      active: false,
+      link: ''
     },
     {
       text: "Pending",
-      link: ""
+      active: false,
+      link: ''
     },
     {
       text: "Closed",
-      link: ""
+      active: false,
+      link: ''
     },
   ]
 
@@ -29,9 +34,9 @@ const complaintsnavbar : NextPage = props => {
     <div className="mx-[35px] mt-[44px] bg-[#0B63C5] rounded-t-[15px] h-[104px] w-[1079px]">
       <div className='mx-[76.5px] text-white py-[23.5px] flex flex-row space-x-[60px]'>
         {complaintsnavbarcontents.map((content) => 
-          <div className='border-2 border-white w-fit h-[57px] rounded-[40px] text-center hoverAnimation'>
+          <div className={`border-2 border-white ${content.active && 'bg-[#fff]'} w-fit h-[57px] rounded-[40px] text-center hoverAnimation`}>
             <a href={content.link}>
-              <p className='py-[15px] px-[40px] text-[18px]'>{content.text}</p>
+              <p className={`py-[15px] px-[40px] text-[18px] ${content.active && 'text-[#000]'}`}>{content.text}</p>
             </a>
           </div>
         )}
