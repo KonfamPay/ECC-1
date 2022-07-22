5cbba01b175471163913697f1b847ffd90f8d7cf
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-07-22 14:59:49 +0100

    Linked the notifications icon to the notifications page

--- Components/DashboardNav/Notifications.tsx
+++ Components/DashboardNav/Notifications.tsx
@@ -1,16 +1,23 @@
+import Link from "next/link";
+
 interface NotificationProps {
   newNotifications: boolean;
 }
 
 const Notifications: React.FC<NotificationProps> = ({ newNotifications }) => {
   return (
-    <div className="relative">
-      <img className="w-[24px] h-[24px]" src="/Images/notificationsIcon.svg" />
-      <div
-        style={{ opacity: newNotifications ? 1 : 0 }}
-        className="transition-[150ms] w-[12px] h-[12px] bg-[#FF0000] rounded-full absolute top-0 right-0"
-      ></div>
-    </div>
+    <Link href="/dashboard/notifications">
+      <div className="relative cursor-pointer">
+        <img
+          className="w-[24px] h-[24px]"
+          src="/Images/notificationsIcon.svg"
+        />
+        <div
+          style={{ opacity: newNotifications ? 1 : 0 }}
+          className="transition-[150ms] w-[12px] h-[12px] bg-[#FF0000] rounded-full absolute top-0 right-0"
+        ></div>
+      </div>
+    </Link>
   );
 };
 export default Notifications;
