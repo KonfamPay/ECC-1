9d0a34d2d2bf00515f18aaad5f2b1e146f377b1e
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-24 16:59:13 +0100

    Chore: Updated modal for mark as resolved

--- pages/dashboard/complaintDetails/index.tsx
+++ pages/dashboard/complaintDetails/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import React from 'react';
 
 const mycomplaints : NextPage = props => {
-  const [showModal, setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = React.useState(true);
   return (
     <div className="mx-[35px] mt-[60px] w-[1079px] h-[827px] rounded-[20px]">
       <div className='bg-[#0B63C5] rounded-t-[20px] h-[78px]'>
@@ -82,46 +82,27 @@ const mycomplaints : NextPage = props => {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-
+            <div className="relative w-auto my-6 mx-auto max-w-3xl ">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#F1F7FE] outline-none focus:outline-none">
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                  <p className="my-4 text-slate-500 text-[20px] font-[400] leading-relaxed">
+                    Are you sure you want to mark this complaint as resolved?
                   </p>
                 </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex space-x-[48px] items-center justify-end p-6">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="text-[#0B63C5] background-transparent px-6 py-2 font-[400] text-[16px] outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Close
+                    Cancel
                   </button>
                   <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-[#0B63C5] text-white active:bg-[#0B63C5] font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Save Changes
+                    Proceed
                   </button>
                 </div>
               </div>
