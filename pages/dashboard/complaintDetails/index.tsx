bb8bab424bc34b62b7dec5c6643afff461a71c15
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-24 16:52:35 +0100

    Chore: Added modal message when resolving a complaint

--- pages/dashboard/complaintDetails/index.tsx
+++ pages/dashboard/complaintDetails/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
+import React from 'react';
 
 const mycomplaints : NextPage = props => {
+  const [showModal, setShowModal] = React.useState(false);
   return (
     <div className="mx-[35px] mt-[60px] w-[1079px] h-[827px] rounded-[20px]">
       <div className='bg-[#0B63C5] rounded-t-[20px] h-[78px]'>
@@ -18,7 +20,9 @@ const mycomplaints : NextPage = props => {
           <div className='w-[348px] h-[93.88px] -mt-[33px]'>
             <p className='text-[20px] font-[500] float-right'>Has this complaint been resolved?</p>
             <div className='bg-[#0B63C5] rounded-[9.375px] w-[150px] float-right mt-[17px]'>
-              <p className='text-[14.06px] text-white py-[12.935px] font-[600] mx-auto text-center'>Mark as resolved</p>
+              <div onClick={() => setShowModal(true)}>
+                <p className='text-[14.06px] text-white py-[12.935px] font-[600] mx-auto text-center'>Mark as resolved</p>
+              </div>
             </div>
           </div>
         </div>
@@ -73,6 +77,59 @@ const mycomplaints : NextPage = props => {
           </div>
         </div>
       </div>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Modal Title
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   )
 }
