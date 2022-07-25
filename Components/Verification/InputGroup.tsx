9b2ad705e53f48d8b4b5bb755933dbf986f5ba8b
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-25 18:12:41 +0100

    Chore: Updated verfication page

--- Components/Verification/InputGroup.tsx
+++ Components/Verification/InputGroup.tsx
@@ -1,6 +1,6 @@
 interface InputGroupProps {
   label: string;
-  placeholder: string;
+  placeholder: string | null;
   value: string;
   setValue: any;
   type: string;
@@ -20,7 +20,7 @@ const InputGroup: React.FC<InputGroupProps> = ({
         value={value}
         onChange={(e) => setValue(e.target.value)}
         className="transition-[150ms] py-[20px] xl:py-[25px] px-[20px] focus:outline-none rounded-[10px] border-2 border-[#C5C5C5] mt-[10px] w-full focus:border-[#0B63C5]"
-        placeholder={placeholder}
+        placeholder={(placeholder) ? (placeholder) : null}
         type={type}
       />
     </div>
--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -15,17 +15,50 @@ const LoginPage: NextPage = () => {
           <p className="text-center text-[20px] font-[300] mt-[30px]">Already have an account? <a className="text-[#0B63C5] cursor-pointer">Login</a></p>
         </div>
         <div className="mt-[99px] mx-[109px]">
-          <div className="grid grid-cols-2">
-            <div>
-              <InputGroup
-                label="First Name"
-                placeholder="Enter First Legal Name"
-                value={fullName}
-                setValue={fullName}
-                type="text"
-              />
-            </div>
-          </div>
+          <div className="grid grid-cols-2 gap-x-[65px] gap-y-[52px]">
+            <InputGroup
+              label="First Name"
+              placeholder="Enter First Legal Name"
+              value={fullName}
+              setValue={fullName}
+              type="text"
+            />
+            <InputGroup
+              label="Date Of Birth"
+              placeholder="DD/MM/YYYY"
+              value={fullName}
+              setValue={fullName}
+              type="date"
+            />
+            <InputGroup
+              label="Adress Line"
+              placeholder="Enter Valid Email Address"
+              value={fullName}
+              setValue={fullName}
+              type="text"
+            />
+            <InputGroup
+              label="Phone Number"
+              placeholder="Enter Phone Number"
+              value={fullName}
+              setValue={fullName}
+              type="text"
+            />
+            <InputGroup
+              label="State"
+              placeholder="Enter Your Residential State"
+              value={fullName}
+              setValue={fullName}
+              type="text"
+            />
+            <InputGroup
+              label="L.G.A"
+              placeholder="Enter Your Local Government Area"
+              value={fullName}
+              setValue={fullName}
+              type="text"
+            />
+          </div>  
         </div>
       </div>
     </>
