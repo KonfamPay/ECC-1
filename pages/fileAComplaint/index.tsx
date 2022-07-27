271cfdaeb8711396e6b15c3b0e807575174cf109
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-27 13:03:03 +0100

    Chore: Added gradient for fileAcomplaint

--- pages/fileAComplaint/index.tsx
+++ pages/fileAComplaint/index.tsx
@@ -9,7 +9,7 @@ const index: NextPage = () => {
       </div>
       <div className="mt-[90px] gird grid-cols-3 gap-x-[100px] mx-[224.5px]">
         <div>
-          <div className="text-[32px] w-[70px] h-[70px] bg-gradient-to-r from-indigo-500 rounded-full items-center justify-center">
+          <div className="text-[32px] w-[70px] h-[70px] bg-gradient-to-b from-[#0B63C5] to-[#073D79] rounded-full items-center justify-center">
             <p className="text-center text-white mx-auto py-[11px]">1</p>
           </div>
         </div>
