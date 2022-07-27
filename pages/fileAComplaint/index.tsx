a62980d3c46358a4ce5e1601e28db9f11a1579c8
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-07-27 13:01:43 +0100

    Chore: Udated fileAcomplaint

--- pages/fileAComplaint/index.tsx
+++ pages/fileAComplaint/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from "next";
-import DashboardCard from "../../Components/DashboardComponents/DashboardCard";
-import NavWrapper from "../../Components/DashboardNav/NavWrapper";
-import ComplaintsCardSection from "../../Sections/ComplaintsCardSection";
 
 const index: NextPage = () => {
   return (
-    <div>
-      <div>
-        <p className="text-[40px] font-[500]">File A Complaint</p>
+    <div className="">
+      <div className="mx-auto text-center mt-[72px]">
+        <p className="text-[40px] font-[500]">File a Complaint</p>
+        <p className="text-[#0B63C5] text-[20px] mt-[20px]">Allow ecommerce complaint help you in your fight aginst online fraud </p>
+      </div>
+      <div className="mt-[90px] gird grid-cols-3 gap-x-[100px] mx-[224.5px]">
+        <div>
+          <div className="text-[32px] w-[70px] h-[70px] bg-gradient-to-r from-indigo-500 rounded-full items-center justify-center">
+            <p className="text-center text-white mx-auto py-[11px]">1</p>
+          </div>
+        </div>
       </div>
     </div>
   );
