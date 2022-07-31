8fd85834551ba346c69c18d87783b7ed15699afa
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-07-31 15:10:07 +0100

    Added the success modal to the 3rd page of file a complaint

--- pages/fileAComplaint/3/index.tsx
+++ pages/fileAComplaint/3/index.tsx
@@ -1,78 +1,132 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import FileAComplaintProcess from '../../../Components/FileAComplaintComponents/FileAComplaintProcess'
+import FileAComplaintProcess from "../../../Components/FileAComplaintComponents/FileAComplaintProcess";
 import NavBar from "../../../Components/NavBar";
 import Footer from "../../../Components/FooterComplaints";
 import FileAComplaintRadio from "../../../Components/FileAComplaintComponents/FileAComplaintRadio";
-
+import SuccessModal from "../../../Components/FileAComplaintComponents/SuccessModal";
 
 const index: NextPage = () => {
   const [resolution, setResolution] = useState("");
+  const [successModalShowing, setSuccessModalShowing] = useState(true);
   return (
     <>
-      <NavBar/>
+      {successModalShowing && (
+        <SuccessModal
+          setSuccessModalShowing={setSuccessModalShowing}
+          grievanceId="423576275442ecc"
+        />
+      )}
+      <NavBar />
       <div className="mx-[100px] mt-[72px] mb-[147px]">
         <div className="mx-auto text-center">
           <p className="text-[40px] font-[500]">File a Complaint</p>
-          <p className="text-[#0B63C5] text-[20px] mt-[20px]">Allow ecommerce complaint help you in your fight aginst online fraud </p>
+          <p className="text-[#0B63C5] text-[20px] mt-[20px]">
+            Allow ecommerce complaint help you in your fight aginst online fraud{" "}
+          </p>
         </div>
         <div className="mt-[90px] mx-[112.25px]">
           <div className="flex flex-row space-x-[100px]">
-            <FileAComplaintProcess number={1} text={'Complaint Info'} active={true}/>
-            <FileAComplaintProcess number={2} text={'Upload Document'} active={true}/>
-            <FileAComplaintProcess number={3} text={'Complaint Info'} active={true}/>
+            <FileAComplaintProcess
+              number={1}
+              text={"Complaint Info"}
+              active={true}
+            />
+            <FileAComplaintProcess
+              number={2}
+              text={"Upload Document"}
+              active={true}
+            />
+            <FileAComplaintProcess
+              number={3}
+              text={"Complaint Info"}
+              active={true}
+            />
           </div>
         </div>
         <div className="mt-[90px]">
           <div>
-            <p className="text-[24px] font-[400]">Kindly let us know what you want the end of this process.</p>
+            <p className="text-[24px] font-[400]">
+              Kindly let us know what you want the end of this process.
+            </p>
           </div>
           <form action="">
             <div className="grid grid-cols-2 gap-x-[20px] gap-y-[58px] mt-[60px]">
               <div className="relative">
-                <FileAComplaintRadio 
-                  iconPath={"refund"} 
-                  title={"A Refund"} 
-                  description={"All expenses incurred will be returned to you"} 
+                <FileAComplaintRadio
+                  iconPath={"refund"}
+                  title={"A Refund"}
+                  description={"All expenses incurred will be returned to you"}
                 />
-                <input type="radio" name="" id="" className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"/>
+                <input
+                  type="radio"
+                  name=""
+                  id=""
+                  className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"
+                />
               </div>
               <div className="relative">
-                <FileAComplaintRadio 
-                  iconPath={"compensation"} 
-                  title={"A Compensation"} 
-                  description={"You will be compensated by the company"} 
+                <FileAComplaintRadio
+                  iconPath={"compensation"}
+                  title={"A Compensation"}
+                  description={"You will be compensated by the company"}
                 />
-                <input type="radio" name="" id="" className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"/>
+                <input
+                  type="radio"
+                  name=""
+                  id=""
+                  className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"
+                />
               </div>
               <div className="relative">
-                <FileAComplaintRadio 
-                  iconPath={"apology"} 
-                  title={"An Apology"} 
-                  description={"You will recieve a formal apology"} 
+                <FileAComplaintRadio
+                  iconPath={"apology"}
+                  title={"An Apology"}
+                  description={"You will recieve a formal apology"}
                 />
-                <input type="radio" name="" id="" className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"/>
+                <input
+                  type="radio"
+                  name=""
+                  id=""
+                  className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"
+                />
               </div>
               <div className="relative">
-                <FileAComplaintRadio 
-                  iconPath={"replacement"} 
-                  title={"A Replacement"} 
-                  description={"Your item will be replaced with the right one"} 
+                <FileAComplaintRadio
+                  iconPath={"replacement"}
+                  title={"A Replacement"}
+                  description={"Your item will be replaced with the right one"}
                 />
-                <input type="radio" name="" id="" className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"/>
+                <input
+                  type="radio"
+                  name=""
+                  id=""
+                  className="w-[30px] h-[30px] absolute right-[30px] bottom-[35%]"
+                />
               </div>
             </div>
             <div className="flex space-x-[26px] mt-[71px]">
-              <input type="radio" name="" id="" className="w-[24px] h-[24px] mt-[3px]"/>
-              <p className="text-[20px]">I agree with the <span className="text-[#0B63C5]"> Terms & Conditions</span></p>
+              <input
+                type="radio"
+                name=""
+                id=""
+                className="w-[24px] h-[24px] mt-[3px]"
+              />
+              <p className="text-[20px]">
+                I agree with the{" "}
+                <span className="text-[#0B63C5]"> Terms & Conditions</span>
+              </p>
             </div>
-            <button className="bg-[#0B63C5] mt-[110px] mx-[334px] w-[572.15px] rounded-[12px] text-[20px] font-[600] text-white py-[22px]" type="submit">
+            <button
+              className="bg-[#0B63C5] mt-[110px] mx-[334px] w-[572.15px] rounded-[12px] text-[20px] font-[600] text-white py-[22px]"
+              type="submit"
+            >
               Continue
             </button>
           </form>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
