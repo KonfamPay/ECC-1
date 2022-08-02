ae960ae2bc4fe6ae81d5192f0f7e911e953b13ef
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-02 11:13:40 +0100

    Chore: Updated faq page and updated search icon directory

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -6,13 +6,19 @@ const Home: NextPage = () => {
   return (
     <div>
       <Navbar/>
-      <div className='mt-[217px] bg-[#F1F7FE]'>
-        <div className='mb-[118px] relative'>
-          <p className='text-[48px] font-[600] text-center'>Frequently Asked Questions</p>
+      <div className='mt-[106px]'>
+        <div className="mx-auto items-center w-full text-center bg-[#F1F7FE]">
+          <p className="pt-[111px] pb-[118px] mx-auto text-[481px] sm:text-4xl font-semibold text-black">Frequently Asked <span className='text-[#0B63C5]'>Questions</span></p>
         </div>
-        <div className='mx-[141px] absolute bottom'>
-          <form action="">
-            <input type="text" name="" id="" className='w-full rounded-[12px]'/>
+        <div className="w-auto h-auto rounded-[12px] mx-[141px] bg-[#fff] -mt-16">
+          <form>
+            <div className='relative'>
+               <img src="./icons/search.svg" className='absolute top-[37.95px] fill-[#0B63C5]' alt="" />{/* Update search color icon */}
+              <input type="text" name="" id="" className='h-[106px] rounded-[12px]'   placeholder=''/>
+              <button className='absolute right-[28px] top-[23.5px] rounded-[12px] text-[20px] bg-[#0A5EBC] px-[48.5px] py-[14.5px]'>
+                <p>Search</p>
+              </button>
+            </div>
           </form>
         </div>
       </div>
