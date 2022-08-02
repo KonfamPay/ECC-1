dc114f23fbbd9cadabe7014d91ab136980790d0a
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-02 19:16:08 +0100

    Chore: Updated wacl

--- pages/writeAComplaintLetter/index.tsx
+++ pages/writeAComplaintLetter/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       <Navbar/>
       <div className='mt-[201px] mx-[100px]'> 
         <p className='text-[40px] text-center'>Complaint Letter Tool</p>
-        <div className='bg-[#F1F7FE] ml-[50px] rounded-[12px]'>
+        <div className='bg-[#F1F7FE] rounded-[12px]'>
           <div className='flex space-x-[50px] w-full mt-[53px]'>
             <img src="./icons/pencil.svg" className='ml-[50px]' alt="" />
             <div className='py-[37px] pr-[42px]'>
@@ -21,7 +21,20 @@ const Home: NextPage = () => {
         </div>
         <form action="" className='mt-[87px]'>
           <div className=''>
-            <input type="text" name="" id="" className='placeholder:text-[#858383] placeholder:text-[20px] text-[20px]'/>
+            <div>
+              <input type="text" name="" id="" className='placeholder:text-[#858383] placeholder:text-[20px] text-[20px] border-none border-b-[12px] border-b-[#858383]'/>
+            </div>
+            <div className='mt-[73px] text-[20px] flex'>
+              <p>On</p>
+              <input type="date" name="" id="" className='w-[141px] ml-[9px] mr-[10px]'/>
+              <p className=''>, I purchased</p>
+              <input type="text" name="" id="" className='w-[230px] ml-[9px] mr-[15px]'/>
+              <p>from</p>
+              <input type="text" name="" id="" className='w-[225px] ml-[9px]'/>
+            </div>
+            <div className='mt-[44px]'>
+              <textarea name="" id="" className='w-full'/>
+            </div>
           </div>
         </form>
       </div>
