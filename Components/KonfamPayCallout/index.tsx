ba156e347df74cb09dc442fc2d133ab6f0b64cac
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-08-10 12:09:49 +0100

    Created the KonfamPay callout component

---
+++ Components/KonfamPayCallout/index.tsx
@@ -0,0 +1,18 @@
+const KonfamPayCallout = () => (
+  <section className="bg-[#0F0F0F] pt-[40px]">
+    <p className="text-white text-[36px] max-w-[600px] text-center mx-auto">
+      You can avoid getting scammed by using a trusted escrow service
+    </p>
+    <p className="text-white text-center mt-[10px]">
+      Avoid getting scammed, Use{" "}
+      <span className="text-[#67B467]">konfam Pay.</span>
+    </p>
+    <div className="flex items-center mt-[37px] flex-col">
+      <button className="px-[88px] text-white py-[22px] bg-[#67B467] rounded-[12px]">
+        Learn more
+      </button>
+      <img className="mt-[42px]" src="/Images/kfpImage.png" />
+    </div>
+  </section>
+);
+export default KonfamPayCallout;
