b7b3b2cef9063a244d38c4d8889acc85af5417c0
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-20 21:47:50 +0100

    Feat: Completed verfication component

--- Components/Verification/InputGroup.tsx
+++ Components/Verification/InputGroup.tsx
@@ -15,11 +15,11 @@ const InputGroup: React.FC<InputGroupProps> = ({
 }) => {
   return (
     <div>
-      <p className="text-[20px]">{label} <span className="text-[#EF2E2E]">*</span></p>
+      <p className="text-[14px] lg:text-[20px]">{label}</p>
       <input
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="transition-[150ms] py-[20px] xl:py-[25px] px-[20px] focus:outline-none rounded-[10px] border-2 border-[#C5C5C5] mt-[10px] w-full focus:border-[#0B63C5]"
+        className="transition-[150ms] py-[12.5px] lg:py-[20px] xl:py-[25px] px-[20px] focus:outline-none rounded-[10px] border-2 border-[#C5C5C5] mt-[10px] w-full focus:border-[#0B63C5] text-[14px] lg:text-[16px] placeholder:text-[14px] lg:placeholder:text-[16px]"
         placeholder={(placeholder) ? (placeholder) : null}
         type={type}
       />
