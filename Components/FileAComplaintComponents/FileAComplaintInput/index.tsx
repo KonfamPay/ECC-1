f9d1f587574ba27e955224b63e5bf5a1c99a397b
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-08-24 19:46:16 +0100

    Changed the color for a required attribute

--- Components/FileAComplaintComponents/FileAComplaintInput/index.tsx
+++ Components/FileAComplaintComponents/FileAComplaintInput/index.tsx
@@ -4,7 +4,7 @@ interface FileAComplaintInputProps {
   value: string;
   setValue: any;
   type: string;
-  nairaSymbol: boolean | null
+  nairaSymbol: boolean | null;
 }
 
 const FileAComplaintInput: React.FC<FileAComplaintInputProps> = ({
@@ -13,23 +13,35 @@ const FileAComplaintInput: React.FC<FileAComplaintInputProps> = ({
   value,
   setValue,
   type,
-  nairaSymbol
+  nairaSymbol,
 }) => {
   return (
     <div>
       <div className="flex flex-row space-x-[13px]">
-        <p className="text-[20px]">{label} <span className="text-[#EF2E2E]">*</span></p>
+        <p className="text-[20px]">
+          {label} <span className="text-[#EF2E2E]">*</span>
+        </p>
         <img src="./icons/fac-help.svg" alt="" />
       </div>
       <div className="relative">
         <input
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          className={`transition-[150ms] py-[20px] xl:py-[25px] ${nairaSymbol ? '' : ''} focus:outline-none rounded-[10px] border-2 border-[#C5C5C5] mt-[10px] w-full focus:border-[#0B63C5] placeholder:text-[#C5C5C5] cursor-pointer ${nairaSymbol ? 'px-[52px]' : 'px-[20px] '}`}
-          placeholder={(placeholder) ? (placeholder) : null}
+          className={`transition-[150ms] py-[20px] xl:py-[25px] ${
+            nairaSymbol ? "" : ""
+          } focus:outline-none rounded-[10px] border-2 border-[#C5C5C5] mt-[10px] w-full focus:border-[#0B63C5] placeholder:text-[#C5C5C5] cursor-pointer ${
+            nairaSymbol ? "px-[52px]" : "px-[20px] "
+          }`}
+          placeholder={placeholder ? placeholder : undefined}
           type={type}
         />
-        {nairaSymbol && <img src="./icons/naira.svg" alt="" className="w-[22px] h-[22px] absolute bottom-[28px] left-[20px]"/>}
+        {nairaSymbol && (
+          <img
+            src="./icons/naira.svg"
+            alt=""
+            className="w-[22px] h-[22px] absolute bottom-[28px] left-[20px]"
+          />
+        )}
       </div>
     </div>
   );
