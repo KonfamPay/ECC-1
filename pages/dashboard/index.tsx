f8e0bdf55c92c44d8e24264545662fc78f4d8279
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-08-25 21:53:53 +0100

    Finished integrating the dashboard with the backend

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,34 +1,63 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import DashboardCard from "../../Components/DashboardComponents/DashboardCard";
 import NavWrapper from "../../Components/DashboardNav/NavWrapper";
 import ComplaintsCardSection from "../../Sections/ComplaintsCardSection";
+import jwt_decode from "jwt-decode";
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
+
+interface User {
+	firstName: string;
+	lastName: string;
+	_id: string;
+	iat: number;
+}
 
 const index: NextPage = (props) => {
-  return (
-    <NavWrapper>
-      <div className="">
-        <div className="bg-[#020D1B] w-full h-[255px] rounded-[20px] flex flex-row relative">
-          <div className="text-white pl-[43px] py-[58.5px]">
-            <p className="text-[40px] leading-[60px] font-[600]">Hi, Voke</p>
-            <p className="text-[24px] leading-[36px] font-[600] w-[503px]">
-              Vendor don do you strong thing again? <br /> We are ready to
-              listen to your complaints
-            </p>
-          </div>
-          <div>
-            <img
-              src="./images/man-freaked-out.png"
-              className="absolute right-[21.28px] -top-[30.54px] h-[285.54px] w-[377.72px]"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="mt-[59px]">
-          <DashboardCard />
-        </div>
-      </div>
-    </NavWrapper>
-  );
+	const [cookie, setCookie] = useCookies(["token"]);
+	const [user, setUser] = useState<User>({
+		firstName: "",
+		lastName: "",
+		_id: "",
+		iat: 0,
+	});
+	const router = useRouter();
+	useEffect(() => {
+		if (!cookie.token) router.replace("/login");
+		else {
+			const userObj = jwt_decode(cookie.token) as User;
+			setUser(userObj);
+			console.log(userObj);
+		}
+	}, []);
+	return (
+		<NavWrapper>
+			<div className="">
+				<div className="bg-[#020D1B] w-full h-[255px] rounded-[20px] flex flex-row relative">
+					<div className="text-white pl-[43px] py-[58.5px]">
+						<p className="text-[40px] leading-[60px] font-[600]">
+							Hi, {user.firstName}
+						</p>
+						<p className="text-[24px] leading-[36px] font-[600] w-[503px]">
+							Vendor don do you strong thing again? <br /> We are
+							ready to listen to your complaints
+						</p>
+					</div>
+					<div>
+						<img
+							src="./images/man-freaked-out.png"
+							className="absolute right-[21.28px] -top-[30.54px] h-[285.54px] w-[377.72px]"
+							alt=""
+						/>
+					</div>
+				</div>
+				<div className="mt-[59px]">
+					<DashboardCard />
+				</div>
+			</div>
+		</NavWrapper>
+	);
 };
 
 export default index;
