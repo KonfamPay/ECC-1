e7a999633d538947402ca531bccd024dadc90048
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-26 02:05:13 +0100

    Feat: Completed howdoesitwork section and component

--- Components/HowItWorksCard/index.tsx
+++ Components/HowItWorksCard/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import IconContainer from "../IconContainer";
 
-interface GoalCardProps {
+interface HowItWorksCardProps {
 	title: string;
 	description: string;
 	icon: string;
 	bgIcon: string;
 }
 
-const HowItWorksCard: React.FC<GoalCardProps> = ({
+const HowItWorksCard: React.FC<HowItWorksCardProps> = ({
 	title,
 	description,
 	icon,
@@ -16,8 +16,10 @@ const HowItWorksCard: React.FC<GoalCardProps> = ({
 }) => {
 	return (
 		<>
-			<div className="bg-[#0B63C5] w-[395px] h-[416px] flex flex-col items-center justify-center text-white text-center px-[27px] rounded-[12px] relative">
-				<div className="w-[114px] h-[114px] rounded-full bg-[] flex items-center justify-center absolute top-[-50px] z-10">
+			<div
+				className={` w-[395px] h-[416px] flex flex-col items-center justify-center text-center px-[27px] rounded-[12px] relative howItWorksCard`}
+			>
+				<div className="w-[114px] h-[114px] rounded-full flex items-center justify-center absolute top-[-50px] z-10">
 					<img
 						src={`../icons/how-it-works-icons/${icon}.svg`}
 						className=""
@@ -25,24 +27,16 @@ const HowItWorksCard: React.FC<GoalCardProps> = ({
 					/>
 				</div>
 				<div className="w-[120px] h-[120px] absolute top-[-53px] bg-white rounded-full z-[1]" />
-        <div className="flex items-center justify-center w-[68px] h-[68px] pointerShadow bg-white rounded-full absolute right-[-33px]">
-          <img src="../icons/how-it-works-icons/pointer.svg" alt="" />
-        </div>
-				<div className="">
+				<div className="flex items-center justify-center w-[68px] h-[68px] pointerShadow bg-white rounded-full absolute right-[-33px] z-[50]">
+					<img src="../icons/how-it-works-icons/pointer.svg" alt="" />
+				</div>
+				<div className="text-white howItWorksCardText">
 					<p className="text-[24px] font-[600]">{title}</p>
 					<p className="text-[20px] mt-[40px] leading-[30px]">
 						{description}
 					</p>
 				</div>
 			</div>
-			{/* <div className="howDoesItWorkCard bg-cover w-full h-full relative">
-				<div className="top-1/2 left-1/2">
-					<p className="text-[24px] font-[600]">{title}</p>
-					<p className="text-[20px] mt-[40px] leading-[30px]">
-						{description}
-					</p>
-				</div>
-			</div> */}
 		</>
 	);
 };
--- Sections/HowDoesItWork/index.tsx
+++ Sections/HowDoesItWork/index.tsx
@@ -3,7 +3,7 @@ import HowItWorksCard from "../../Components/HowItWorksCard";
 
 const HowDoesItWork: React.FC = (props) => {
 	return (
-		<div className="mt-[119px]">
+		<div className="mt-[119px] mb-[62px]">
 			<div className="flex flex-col space-y-[30px] items-center justify-center mx-auto">
 				<p className="font-[600] text-[48px] leading-[72px]">
 					How Does It <span className="text-[#0B63C5]">Work?</span>
@@ -12,15 +12,59 @@ const HowDoesItWork: React.FC = (props) => {
 					You will only need to file a complaint and we will handle
 					the remaining processes{" "}
 				</p>
-				<div>
-					<HowItWorksCard
-						title={"File a Complaint"}
-						description={
-							"Get resolution with minimum efforts and reclaim your family time."
-						}
-						icon={"file"}
-						bgIcon={"fff"}
-					/>
+				<div className="pt-[85px] flex flex-col space-y-[100px]">
+					<div className="flex space-x-[19px] howItWorksContent1">
+						<HowItWorksCard
+							title={"File a Complaint"}
+							description={
+								"Get resolution with minimum efforts and reclaim your family time."
+							}
+							icon={"file"}
+							bgIcon={"fff"}
+						/>
+						<HowItWorksCard
+							title={"Notify the Company"}
+							description={
+								"Get resolution with minimum efforts and reclaim your family time."
+							}
+							icon={"notify"}
+							bgIcon={"#fff"}
+						/>
+						<HowItWorksCard
+							title={"Start social media campaign"}
+							description={
+								"Get resolution with minimum efforts and reclaim your family time."
+							}
+							icon={"speak"}
+							bgIcon={"#fff"}
+						/>
+					</div>
+					<div className="flex space-x-[19px] howItWorksContent2">
+						<HowItWorksCard
+							title={"Send legal notice"}
+							description={
+								"Get resolution with minimum efforts and reclaim your family time."
+							}
+							icon={"legal"}
+							bgIcon={"fff"}
+						/>
+						<HowItWorksCard
+							title={"Notify the Company"}
+							description={
+								"Get resolution with minimum efforts and reclaim your family time."
+							}
+							icon={"notify"}
+							bgIcon={"#fff"}
+						/>
+						<HowItWorksCard
+							title={"Start social media campaign"}
+							description={
+								"Get resolution with minimum efforts and reclaim your family time."
+							}
+							icon={"speak"}
+							bgIcon={"#fff"}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
