052cd800df46f4e9b322b32544c4931aa3735d4f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-09 22:27:28 +0100

    Changed the cookie stored from token to user

--- Components/DashboardNav/TopNav.jsx
+++ Components/DashboardNav/TopNav.jsx
@@ -4,22 +4,22 @@ import { useCookies } from "react-cookie";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import moment from "moment";
-import jwt_decode from "jwt-decode";
 
 const TopNav = () => {
-	const [cookie, setCookie] = useCookies(["token"]);
+	const [cookie, setCookie] = useCookies(["user"]);
     const router = useRouter();
     const [user, setUser] = useState({});
     const currentDate = moment().format("ddd. Do MMMM, YYYY");
 	useEffect(() => {
-		if (!cookie.token) router.replace("/login");
-		else {
-			const user = jwt_decode(cookie.token);
-			setUser(user);
+		if (!cookie.user) router.replace("/login");
+		else {			
+            setUser(cookie.user);
+            console.log(user);
 		}
-	}, []);
+    }, []);
+    console.log(new Date(user.createdAt).getFullYear());
     return (
-        <div style={{zIndex: 20}} className="fixed left-[274px] top-0 w-[calc(100%-274px)] h-[126px] bg-white pl-[35px] pt-[26px] pr-[66px] flex justify-between items-center poppinsFont">
+        <div style={{zIndex: 20}} className="fixed left-[274px] top-0 w-[calc(100%-274px)] h-[110px] bg-white pl-[35px] pt-[0px] pr-[66px] flex justify-between items-center poppinsFont">
             <div>
                 <p className="font-medium text-[24px] text-[#0B63C5]">Dashboard</p>
                 <p className="font-medium text-[18px] text-[#7A797D] mt-[2px]">{currentDate}</p>
@@ -27,12 +27,12 @@ const TopNav = () => {
             <div className="flex items-center gap-x-[60px]">
                     <Notifications newNotifications/>
                 <div className="flex gap-x-[21px]">
-                    <div className="w-[50px] h-[50px] rounded-full">
-                        <img className="w-full" src="/Images/profilePic.png" />
+                    <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                        <img className="w-full" src={user.profilePic || "/Images/profilePic.png"} />
                     </div>
                     <div className="flex flex-col items-start ">
                         <p className="font-semibold opacity-70 text-[20px]">{`${user.firstName} ${user.lastName}`}</p>
-                        <p className="font-semibold text-[14px] relative "><span className="opacity-70">Member since</span> <span className="text-[#0B63C5]">2022</span></p>
+                        <p className="font-semibold text-[14px] relative "><span className="opacity-70">Member since</span> <span className="text-[#0B63C5]">{new Date(user.createdAt).getFullYear()}</span></p>
                     </div>
                 </div>
             </div>
