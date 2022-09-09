8e1b9f45b349ceb2204ce8794114e56f8c3eae70
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-09 22:28:58 +0100

    Created a file for adding the validation functions

---
+++ Components/Verification/FormValidation.js
@@ -0,0 +1,21 @@
+import Joi from "joi-browser";
+
+export const validateVerifyInputs = (payload) => {
+	const schema = Joi.object({
+		phoneNumber: Joi.string()
+			.regex(/^[0-9]{11}$/)
+			.length(11)
+			.label("Phone Number")
+			.required(),
+		dob: Joi.string().required().label("Date of Birth").length(10),
+		state: Joi.string().max(30).min(3).required().label("State"),
+		lga: Joi.string()
+			.max(50)
+			.min(3)
+			.required()
+			.label("Local Government Area"),
+		address: Joi.string().max(255).min(10).required().label("Address"),
+		photoIdUrl: Joi.string().required().label("Photo Id"),
+	});
+	return schema.validate(payload, { abortEarly: false });
+};
